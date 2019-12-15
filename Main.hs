c5b6ff2e443090383a24ae4878022017f3ee8b90
jakerosen <jakesrosen@gmail.com>
2019-12-14 17:58:57 -0800

    Finished day1

--- Main.hs
+++ Main.hs
@@ -1,4 +1,44 @@
 module Main where
 
+import Data.Either
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Read as T
+import Control.Lens
+
 main :: IO ()
-main = print (5 :: Int)
+main = day1p2
+
+day1p1 :: IO ()
+day1p1 = do
+  masses :: [Int] <- map tToI <$> readLines "data/day1-1"
+  let
+    ans = sum $ map fuel masses
+  print ans
+
+day1p2 :: IO ()
+day1p2 = do
+  masses :: [Int] <- map tToI <$> readLines "data/day1-1"
+  let
+    ans = sum $ map fuel2 masses
+  print ans
+
+fuel :: Int -> Int
+fuel m = if x > 0 then x else 0
+  where
+    x = m `div` 3 - 2
+
+fuel2 :: Int -> Int
+fuel2 m = if x > 0
+  then x + fuel2 x
+  else x
+  where
+    x = fuel m
+
+
+readLines :: FilePath -> IO [Text]
+readLines file = T.lines <$> T.readFile file
+
+tToI :: Integral a => Text -> a
+tToI t = (fromRight undefined $ T.decimal t) ^. _1
